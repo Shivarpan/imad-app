503df8ce714051feac10c756bf34598e44498f14
Shivarpan <36412741+Shivarpan@users.noreply.github.com>
2018-04-19 20:37:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/test-db',function(req,res){
+    
+    //make a select request
+    //return a response
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
